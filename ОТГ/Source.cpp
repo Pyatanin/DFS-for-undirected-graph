4304de6d017591caf206c400b20608f4519cecfd
PM93-Korzhov-Kirill <PM93.Korzhov@gmail.com>
2020-04-25 13:36:58 +0700

    refactoring by Korzhov

--- ОТГ/Source.cpp
+++ ОТГ/Source.cpp
@@ -1,10 +1,12 @@
-// Àëãîðèòìû ïîèñêà â øèðèíó è ãëóáèíó.
+// Алгоритмы поиска в ширину и глубину.
 //
-// 7. Îïðåäåëèòü, ñóùåñòâóåò ëè ìàðøðóò ìåæäó äâóìÿ çàäàííûìè âåðøèíàìè â íåîðèåíòèðîâàííîì ãðàôå. 
-// Ãðàô â ïàìÿòè ïðåäñòàâëÿòü â âèäå ìàòðèöû.
+// 7. Определить, существует ли маршрут между двумя заданными вершинами в неориентированном графе. 
+// Граф в памяти представлять в виде матрицы.
 // 
-// Âûïîëíèëè: Ïÿòàíèí À., Êîðæîâ Ê., Èøóòèí Ì., Áóðëàêîâ Â. (ÏÌ-93)
-// Àâòîð: Ïÿòàíèí À.
+// Обход в глубину.
+// Выполнили: Пятанин А., Коржов К., Ишутин М., Бурлаков В. (ПМ-93)
+// Автор: Пятанин А.
+// Рефакторинг: Коржов К.
 
 #include <iostream>
 #include <fstream>
@@ -12,14 +14,20 @@
 #include <chrono>
 #include <vector>
 
-std::vector<bool> marks;
 
-void doDfs(size_t source, size_t destination, const std::vector<std::vector<double>> &graph)
+
+bool doDfs(size_t sourceVertex, size_t targetVertex, const std::vector<std::vector<size_t>> &graph, std::vector<bool> &marks)
 {
-   marks[source] = true;
-   for (size_t i = 0; i < graph[source].size() && marks[destination] != true; i++) // Такое условие чтобы не просматривал
-      if (!marks[i] && graph[source][i]!= 0)                                       //  все вершины если уже нашел путь.
-         doDfs(i, destination, graph);
+   if (sourceVertex == targetVertex)
+      return true;
+   marks[sourceVertex] = true;
+
+   for (size_t iVertex = 0; iVertex < graph[sourceVertex].size(); iVertex++)
+      if (!marks[iVertex] && graph[sourceVertex][iVertex])
+         if (doDfs(iVertex, targetVertex, graph, marks))
+            return true;
+
+   return false;
 }
 
 int main()
@@ -30,30 +38,32 @@ int main()
    std::fstream in("in.txt");
    std::ofstream out("out.txt");
 
-   size_t n = 0;
-   in >> n;
-   std::vector<std::vector<double>> graph(n, std::vector<double>(n, -1));
+   size_t vertexCount = 0;
+   in >> vertexCount;
+   std::vector<std::vector<size_t>> graph(vertexCount, std::vector<size_t>(vertexCount, -1));
 
-   for (size_t i = 0; i < n; i++)
-      for (size_t j = 0; j < n; j++)
+   for (size_t i = 0; i < vertexCount; i++)
+      for (size_t j = 0; j < vertexCount; j++)
          in >> graph[i][j];
 
-   size_t source = 0, destination = 0;
-   in >> source >> destination;
-   source--;
-   destination--;
+   size_t sourceVertex = 0, targetVertex = 0;
+   in >> sourceVertex >> targetVertex;
+   sourceVertex--;
+   targetVertex--;
 
-   marks.assign(n, false);
-
+   std::vector<bool> marks(graph[sourceVertex].size(), false);
+   
    auto startTime = std::chrono::high_resolution_clock::now();
-   doDfs(source, destination, graph);
+
+   bool routeFound = doDfs(sourceVertex, targetVertex, graph, marks);
+   
    auto endTime = std::chrono::high_resolution_clock::now() - startTime;
    auto elapsedTime = std::chrono::duration<double>(endTime).count();
 
-   if (marks[destination])
-      out << "Route exists.\n";
+   if (routeFound)
+      out << "Route exists." << std::endl;
    else
-      out << "Route is absent.\n";
+      out << "Route is absent." << std::endl;
 
    out << "Algorithm time is " << elapsedTime << " seconds."<<std::endl;
 
